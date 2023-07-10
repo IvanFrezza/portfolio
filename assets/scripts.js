15d51c316169dde88335c11da719dbe4351fa50a
Ivan Frezza <ivan@pond5.com>
2023-07-10 22:16:35 +0100

    feat: add job page structure

--- assets/scripts.js
+++ assets/scripts.js
@@ -4,6 +4,24 @@ menu.forEach(element => {
 });
 
 
+const jobmenu = document.getElementById("jobmenu").querySelectorAll("a");
+jobmenu.forEach(element => {
+    element.addEventListener("click", changeJobFrame, false);
+});
+
+
+function changeJobFrame(event) {
+    event.preventDefault();
+
+    jobmenu.forEach(element => {
+        element.classList.remove('activejob');
+        
+    });
+
+    this.classList.add('activejob');
+}
+
+
 // Function to change the content of t2
 function changeFrame(event) {
     // event.preventDefault();
